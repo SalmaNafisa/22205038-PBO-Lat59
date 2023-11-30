3e54c95a190dffc642b25d429633b1908cfdb631
SALMA NAFISA QURROTU'AINI <145917431+SalmaNafisa@users.noreply.github.com>
2023-11-30 12:11:59 +0700

    Add files via upload

---
+++ DetektifConan59/src/detektifconan59/Karakter.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package detektifconan59;
+
+/**
+ *
+ * @author ASUS TUF
+ */
+public class Karakter {
+    private String nama;
+    private String peran;
+
+    public Karakter(String nama, String peran) {
+        this.nama = nama;
+        this.peran = peran;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getPeran() {
+        return peran;
+    }
+
+}
